011269dc826568c46c6e02aa217eab42922dec42
tjohnny93 <tjohnny93@berkeley.edu>
2019-05-28 14:32:16 -0700

    Create Tables

---
+++ AA_Question/import_db.sql
@@ -0,0 +1,50 @@
+PRAGMA foreign__keys = ON;
+
+CREATE TABLE users
+(
+  id INTEGER PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions
+(
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows
+(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  parent_reply_id INTEGER,
+  author_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES question(id)
+);
+
+INSERT
